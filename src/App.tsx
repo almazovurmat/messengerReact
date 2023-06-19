ea9487a7d001a0bff78bf40093d63169113d44c2
Almazov Urmat <almazov.urmat@gmail.com>
2023-06-19 09:27:41 +0600

    Created components MessengerCard and MessengerForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Messenger from "./containers/Messenger/Messenger";
 import './App.css';
 
 const App = () => (
     <div className="App">
-
+        <div className="container">
+            <Messenger/>
+        </div>
     </div>
 );
 
